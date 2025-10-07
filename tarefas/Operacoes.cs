9ed08d4599981d7ad597275f797142027d742e6a
VictorVerissimo18 <victorverissimo2618@gmail.com>
2025-10-07 00:27:31 +0000

    feat statement

---
+++ tarefas/Operacoes.cs
@@ -0,0 +1,27 @@
+public class Operacoes
+{
+    public id int (Tarefas; tarefa);
+    {
+    return 0;
+    }
+    
+    public Tarefa Buscar(int id)
+    {   
+    return null;
+    }    
+
+    public List<Tarefas> Listar()
+    {
+    return Array.Empty<Tarefa>();
+    }
+
+    public void Alterar(Tarefa tarefa)
+    {
+
+    }
+
+    public void Excluir(int id)
+    {
+
+    }
+}
