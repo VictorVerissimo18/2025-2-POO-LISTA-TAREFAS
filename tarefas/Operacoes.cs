0f9748fb347ae2d9d3d1e7a9b49753340d2f841c
VictorVerissimo18 <victorverissimo2618@gmail.com>
2025-10-20 23:21:29 +0000

    feat: statement

--- tarefas/Operacoes.cs
+++ tarefas/Operacoes.cs
@@ -2,13 +2,14 @@ using MySql.Data.MySqlClient;
 
 public class Operacoes
 {
-    private string connectionString = @"server=phpmyadmin.uni9.marize.us;User ID=user_poo;password=S3nh4!F0rt3;database=user_poo;";
+    private string connectionString = 
+    @"server=phpmyadmin.uni9.marize.us;User ID=user_poo;password=S3nh4!F0rt3;database=user_poo;";
     public int Criar(Tarefa tarefa)
     {
         using(var conexao = new MySqlConnection(connectionString))
         {
             conexao.Open();
-            string sql = @"INSERT INTO tarefa (nome, descricao, dataCriacao, status, dataExecucao)
+            string sql = @"INSERT INTO tarefa (nome, descricao, dataCriacao, status, dataExecucao) 
                            VALUES (@nome, @descricao, @dataCriacao, @status, @dataExecucao);
                            SELECT LAST_INSERT_ID();";
             using (var cmd = new MySqlCommand(sql, conexao))
@@ -29,7 +30,7 @@ public class Operacoes
         return null;
     }
 
-    public List<Tarefa> Listar()
+    public IList<Tarefa> Listar()
     {
         return Array.Empty<Tarefa>();
     }
@@ -38,8 +39,9 @@ public class Operacoes
     {
 
     }
+
     public void Excluir(int id)
     {
 
     }
-    }
+}
--- tarefas/Program.cs
+++ tarefas/Program.cs
@@ -1,10 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Preenchendo dados tarefa 01");
 var tarefa01 = new Tarefa();
 tarefa01.Nome = "Fazer compras";
-tarefa01.Descricao = "Comprar arroz, feijao e frutas";
-tarefa01.DataCriacao = DateTime.Now;
+tarefa01.Descricao  = "Comprar arroz, feijão e frutas";
+tarefa01.DataCriacao = DateTime.Now; 
 tarefa01.Status = 1;
 tarefa01.DataExecucao = null;
 
@@ -15,4 +16,4 @@ Console.WriteLine("Inserindo dados no banco de dados");
 var operacoes = new Operacoes();
 int idInserido = operacoes.Criar(tarefa01);
 
-Console.WriteLine($"Dados inseridos no banco de dados com sucesso. Id: (idInserido)");
+Console.WriteLine($"Dados inseridos no banco de dados com sucesso. Id: {idInserido}");
