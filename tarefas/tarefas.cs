c0ba5ba3ab32530a3035df24eb3d4001b285ce34
VictorVerissimo18 <victorverissimo2618@gmail.com>
2025-10-20 23:09:36 +0000

    feat: statement

--- tarefas/tarefas.cs
+++ tarefas/tarefas.cs
@@ -1,31 +1,11 @@
-// tarefas.cs
-
-using System;
-
-public class Tarefas
+public class Tarefa
 {
-    public int id { get; set; }
-    public string nome { get; set; }
-    public string descricao { get; set; }
-    public DateTime dataCriacao { get; set; }
-    public int status { get; set; }
-    public DateTime dataExecucao { get; set; }
 
-    // Construtor padrão
-    public Tarefas()
-    {
-        // Inicializa a data de criação e o status
-        this.dataCriacao = DateTime.Now;
-        this.status = 0; // Ex: 0 = Pendente
-    }
+public int Id {get; set;}
+public string Nome {get; set;}
+public string Descricao {get; set;}
+public DataTime DataCriacao {get; set;}
+public int Status {get; set;}
+public DateTime DataExecucao {get; set;}
 
-    // Construtor para inicializar as propriedades principais
-    public Tarefas(int id, string nome, string descricao)
-    {
-        this.id = id;
-        this.nome = nome;
-        this.descricao = descricao;
-        this.dataCriacao = DateTime.Now;
-        this.status = 0;
-    }
 }
